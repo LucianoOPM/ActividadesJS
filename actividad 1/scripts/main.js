bbd221cc4a08622afa66646ffd59483514ee016c
Luciano Perez Montalvan <jesus-61914@hotmail.com>
2022-11-28 14:39:41 -0600

    Primera actividad del workbook de JS

---
+++ actividad 1/scripts/main.js
@@ -0,0 +1,36 @@
+/* 
+let peticion = Number(prompt("Ingrese un numero a sumar"))
+let sumar = 5
+
+do{
+    peticion = peticion + sumar
+    console.log(peticion)
+}while(peticion < 100)
+
+for(let x = 1; x <= 10; x+=1){
+    console.log(x)
+}
+
+while(peticion <= 100){
+    peticion = peticion + sumar
+    console.log(peticion)
+}
+*/
+
+/* 
+let text = prompt("Ingrese su texto a concatenar, 'ESC' para terminar de concatenar")
+let texto = ""
+do{
+    texto = texto + " " + text
+    alert(`${texto}`)
+    text = prompt("Escriba su nuevo mensaje, 'ESC' para cancelar")
+}while(text != "ESC") 
+*/
+
+/* 
+let cantidad = Number(prompt("¿Cuantas veces desea repetir el mensaje?"))
+
+for(let i = 1; i <= cantidad; i+=1){
+    alert("hola")
+} 
+*/
